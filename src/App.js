7fd9d1f7e21f0036b977d9b77d04e663f0598260
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2021-12-13 00:59:02 -0300

    UPDATE: Form refactoring

--- src/App.js
+++ src/App.js
@@ -1,51 +1,19 @@
 import { useState } from "react";
-import useFormulario from "./hooks/useFormulario";
-import Input from "./components/Input";
 import Card from "./components/Card";
 import Container from "./components/Container";
-import Button from "./components/Button";
+import UserForm from "./components/UserForm";
 function App() {
   const [usuarios, setUsuarios] = useState([]);
-  const [formulario, handleChange, reset] = useFormulario({
-    name: "",
-    lastname: "",
-    email: "",
-  });
 
-  const submit = (e) => {
-    e.preventDefault();
-    setUsuarios([...usuarios, formulario]);
-    reset();
+  const submit = (usuario) => {
+    setUsuarios([...usuarios, usuario]);
   };
   return (
     <div style={{ marginTop: "10%" }}>
       <Container>
         <Card>
           <div style={{ padding: 20 }}>
-            <form onSubmit={submit}>
-              <Input
-                label="Nombre"
-                name="name"
-                value={formulario.name}
-                onChange={handleChange}
-                placeholder="Nombre"
-              />
-              <Input
-                label="Apellido"
-                name="lastname"
-                value={formulario.lastname}
-                onChange={handleChange}
-                placeholder="Apellido"
-              />
-              <Input
-                label="Correo"
-                name="email"
-                value={formulario.email}
-                onChange={handleChange}
-                placeholder="Correo"
-              />
-              <Button>Enviar</Button>
-            </form>
+            <UserForm submit={submit} />
           </div>
         </Card>
         <Card>
