ea49a591db1249f974989bf3683cb74c235051c5
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2021-12-12 23:05:09 -0300

    ADD: custom hook useFormulario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import "./App.css";
+import useFormulario from "./hooks/useFormulario";
 
 function App() {
+  const [formulario, handleChange] = useFormulario({ name: "" });
+
+  console.log(formulario);
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Gestor de usuarios</h1>
-      </header>
-    </div>
+    <form>
+      <input
+        name="name"
+        placeholder="Nombre"
+        value={formulario.name}
+        onChange={handleChange}
+      />
+    </form>
   );
 }
 
