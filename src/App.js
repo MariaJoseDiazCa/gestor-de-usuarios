851c2a396198cc70109c18bf2e0ea0d8645c8093
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2021-12-12 22:47:18 -0300

    ADD: create-react-app project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Gestor de usuarios</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
